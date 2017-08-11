3963f5de372d8e852db97542824c2109c01b4312
Nidhi Paneri <Nidhi.Paneri@Advaiya.com>
2017-08-11 18:41:08 +0530

    Added source

---
+++ scripts/custom/core.js
@@ -0,0 +1,345 @@
+﻿
+(function () {
+    "use strict";
+
+    One2Teamapp.AppViewModel = (function () {
+
+        var docs = ko.observableArray();
+        var breadCrumbs = ko.observableArray();
+        var keepGoing = true;
+        var timerid;
+        var tempfolderUrl = null;
+        var breadcrumbIndex = null;
+        var addBreadcrumb = null;
+        var isTokenRefreshed = false;
+
+        setTimeout(function () {
+            keepGoing = false;
+        }, One2Teamapp.Param.timeOutToGetAccessToken);
+
+        function GetToken() {
+            try {
+                if (One2Teamapp.Param.domain &&
+                One2Teamapp.Param.appWebUrl &&
+                One2Teamapp.Param.documentUrl &&
+                One2Teamapp.Param.applicationId &&
+                One2Teamapp.Param.clientsecret &&
+                One2Teamapp.Param.redirectUrl) {
+                    keepGoing = true;
+                    timerid = setTimeout(CheckToken, 1000);
+                    window.open(One2Teamapp.Param.msLoginUrl + One2Teamapp.Param.domain + '/oauth2/authorize?resource=' + One2Teamapp.Param.appWebUrl + '&client_id=' + One2Teamapp.Param.applicationId + '&response_type=id_token+code&redirect_uri=' + One2Teamapp.Param.redirectUrl + '&response_mode=form_post&scope=openid&state=12345&nonce=7362CAEA-9CA5-4B43-9BA3-34D7C303EBA7');
+                }
+                else {
+                    One2Teamapp.Utility.RestApiCall(One2Teamapp.Param.urlToGetconfigParam, "GET", One2Teamapp.Param.headerToGetconfigParam, function (callback) {
+                        if (callback.status && callback.status == 'error') {
+                            alert(One2Teamapp.Utility.customMessage.ConfigParamError);
+                        }
+                        else {
+                            One2Teamapp.Param.domain = callback.domain;
+                            One2Teamapp.Param.appWebUrl = callback.appWebUrl;
+                            One2Teamapp.Param.documentUrl = callback.documentUrl;
+                            One2Teamapp.Param.applicationId = callback.applicationId;
+                            One2Teamapp.Param.clientsecret = callback.clientSecret;
+                            One2Teamapp.Param.redirectUrl = callback.redirectUrl;
+
+                            keepGoing = true;
+                            timerid = setTimeout(CheckToken, 1000);
+                            window.open(One2Teamapp.Param.msLoginUrl + One2Teamapp.Param.domain + '/oauth2/authorize?resource=' + One2Teamapp.Param.appWebUrl + '&client_id=' + One2Teamapp.Param.applicationId + '&response_type=id_token+code&redirect_uri=' + One2Teamapp.Param.redirectUrl + '&response_mode=form_post&scope=openid&state=12345&nonce=7362CAEA-9CA5-4B43-9BA3-34D7C303EBA7');
+                        }
+                    });
+                }
+            }
+            catch (e) {
+                alert(One2Teamapp.Utility.customMessage.ConfigParamError);
+            }
+        }
+
+        function GetRefreshToken() {
+            localStorage.setItem("accesstoken", null);
+            isTokenRefreshed = true;
+            keepGoing = true;
+            timerid = setTimeout(CheckToken, 1000);
+            window.open('https://login.microsoftonline.com/' + One2Teamapp.Param.domain + '/oauth2/authorize?resource=' + One2Teamapp.Param.appWebUrl + '&client_id=' + One2Teamapp.Param.applicationId + '&response_type=id_token+code&redirect_uri=' + One2Teamapp.Param.redirectUrl + '&response_mode=form_post&refresh_token=' + localStorage.getItem("refreshtoken") + '&scope=openid&state=12345&nonce=7362CAEA-9CA5-4B43-9BA3-34D7C303EBA7');
+        }
+
+        function Logout() {
+            localStorage.setItem("accesstoken", null);
+            window.open('https://login.microsoftonline.com/' + One2Teamapp.Param.domain + '/oauth2/logout');
+        }
+
+        function UploadToSharepoint() {
+            tempfolderUrl = breadCrumbs()[breadCrumbs().length - 1].url;
+            window.open(One2Teamapp.Param.appWebUrl + One2Teamapp.Param.documentUrl + '/Forms/AllItems.aspx?RootFolder=' + tempfolderUrl);
+        }
+
+        function UploadToSPhere() {
+            tempfolderUrl = breadCrumbs()[breadCrumbs().length - 1].url;
+            One2Teamapp.Utility.UploadDocument(tempfolderUrl);
+        }
+
+        function CreateNewFolder() {
+            var folderName = prompt(One2Teamapp.Utility.customMessage.enterFolerName, "");
+            if (folderName.trim()) {
+                tempfolderUrl = breadCrumbs()[breadCrumbs().length - 1].url;
+                var url = "/_api/web/getfolderbyserverrelativeurl('" + tempfolderUrl + "')/Folders/Add('" + folderName.trim() + "')";
+                var header = {
+                    "Authorization": "Bearer " + localStorage.getItem("accesstoken"),
+                    "accept": "application/json;odata=verbose",
+                    "content-type": "application/json; odata=verbose",
+                    "X-RequestDigest": jQuery("#__REQUESTDIGEST").val()
+                };
+                One2Teamapp.Utility.RestApiCall(url, "POST", header, function (callback) {
+                    if (callback.status && callback.status == 'error') {
+                        alert(One2Teamapp.Utility.customMessage.newFolderError + callback.errormessage);
+                    }
+                    else {
+                        GetItems(tempfolderUrl);
+                    }
+                });
+            }
+            else {
+                alert(One2Teamapp.Utility.customMessage.folderNameError);
+                return false;
+            }
+        }
+
+        function CheckToken() {
+            if (localStorage.getItem("accesstoken") != "" && localStorage.getItem("accesstoken") != null && localStorage.getItem("accesstoken") != "null") {
+                AbortTimer();
+                if (tempfolderUrl != null) {
+                    if (breadcrumbIndex != null) {
+                        breadCrumbs.splice(item.index);
+                        breadcrumbIndex = null;
+                    }
+                    if (addBreadcrumb != null) {
+                        AddCrumb(addBreadcrumb.ServerRelativeUrl, addBreadcrumb.Name, true, breadCrumbs().length + 1);
+                        addBreadcrumb = null;
+                    }
+                    GetItems(tempfolderUrl);
+                    tempfolderUrl = null;
+                }
+                else {
+                    GetFoldersAndFiles();
+                }
+            }
+            else if (keepGoing)
+                timerid = setTimeout(CheckToken, 1000); // repeat
+            else {
+                $("#wait").css("display", "none");
+                alert(One2Teamapp.Utility.customMessage.docLibOpenError);
+                AbortTimer();
+            }
+        }
+
+        function AbortTimer() {
+            clearTimeout(timerid);
+        }
+
+        function OnFolderClick(item) {
+            if (localStorage.getItem("accesstoken") != null && localStorage.getItem("accesstoken") != "null" && localStorage.getItem("accesstoken") != "") {
+                AddCrumb(item.ServerRelativeUrl, item.Name, true, breadCrumbs().length + 1);
+                GetItems(item.ServerRelativeUrl);
+            }
+            else {
+                tempfolderUrl = item.url;
+                addBreadcrumb = item;
+                GetToken();
+            }
+        }
+
+        function OnBreadCrumbClick(item) {
+            if (localStorage.getItem("accesstoken") != null && localStorage.getItem("accesstoken") != "null" && localStorage.getItem("accesstoken") != "") {
+                breadCrumbs.splice(item.index);
+                GetItems(item.url);
+            }
+            else {
+                tempfolderUrl = item.url;
+                breadcrumbIndex = item.index;
+                GetToken();
+            }
+        }
+
+        function AddCrumb(url, text, active, index) {
+            breadCrumbs.push({
+                'url': url, 'text': text, 'active': active, 'index': index
+            });
+        }
+
+        function GetFoldersAndFiles() {
+            if (localStorage.getItem("accesstoken") != null && localStorage.getItem("accesstoken") != "null" && localStorage.getItem("accesstoken") != "") {
+                $("#wait").css("display", "block");
+                breadCrumbs([]);
+                AddCrumb(One2Teamapp.Param.documentUrl, "Documents", true, 1);
+                GetItems(One2Teamapp.Param.documentUrl);
+                $("#docModal").modal('show');
+            }
+            else {
+                GetToken();
+            }
+        }
+
+        function GetItems(folderUrl) {
+            tempfolderUrl = null;
+            if (localStorage.getItem("accesstoken") != null && localStorage.getItem("accesstoken") != "null" && localStorage.getItem("accesstoken") != "") {
+                $("#wait").css("display", "block");
+
+                var header = {
+                    "Authorization": "Bearer " + localStorage.getItem("accesstoken"),
+                    "Access-Control-Allow-Origin": "*",
+                    "accept": "application/json;odata=verbose"
+                };
+                var url = "/_api/web/GetFolderByServerRelativeUrl('" + folderUrl + "')?$expand=Folders,Files";
+                One2Teamapp.Utility.RestApiCall(url, "GET", header, function (callback) {
+                    if (callback.status && callback.status == 'error') {
+                        if (!isTokenRefreshed)
+                            GetRefreshToken();
+                        else {
+                            $("#wait").css("display", "none");
+                            alert(One2Teamapp.Utility.customMessage.docLibOpenError + callback.errormessage);
+                            isTokenRefreshed = false;
+                        }
+                    }
+                    else {
+                        docs([]);
+                        $.each(callback.Folders.results, function (obj, item) {
+                            var folder = {
+                                Name: item.Name,
+                                IsFolder: true,
+                                URL: One2Teamapp.Param.appWebUrl + "/" + item.ServerRelativeUrl,
+                                ServerRelativeUrl: item.ServerRelativeUrl,
+                                isSelected: false,
+                                modified: item.TimeLastModified,
+                                icon: One2Teamapp.Utility.GetIcon(item.Name.substr(item.Name.lastIndexOf('.') + 1))
+                            };
+                            docs.push(folder);
+                        });
+                        $.each(callback.Files.results, function (obj, item) {
+                            var file = {
+                                Name: item.Name,
+                                IsFolder: false,
+                                URL: item.LinkingUri == null ? One2Teamapp.Param.appWebUrl + "/" + item.ServerRelativeUrl : item.LinkingUri,
+                                ServerRelativeUrl: item.ServerRelativeUrl,
+                                isSelected: false,
+                                modified: item.TimeLastModified,
+                                icon: One2Teamapp.Utility.GetIcon(item.Name.substr(item.Name.lastIndexOf('.') + 1))
+                            };
+                            docs.push(file);
+                        });
+                        // sort by date modified using moment.js
+                        docs.sort(function (left, right) {
+                            return moment.utc(left.modified).diff(moment.utc(right.modified))
+                        }).reverse();
+                        // sort by IsFolder: all folders on top
+                        docs.sort(function (x, y) {
+                            return (x.IsFolder === y.IsFolder) ? 0 : x.IsFolder ? -1 : 1;
+                        });
+                        $("#wait").css("display", "none");
+                    }
+                });
+            }
+            else {
+                tempfolderUrl = folderUrl;
+                GetToken();
+            }
+        }
+
+        function AddToList() {
+            var docsToAdd = [];
+            $.each(docs(), function (obj, item) {
+                if (item.isSelected)
+                    docsToAdd.push({
+                        'Name': item.Name, 'URL': item.URL
+                    });
+            });
+            //This JSON will be returned to One2Team page
+            alert(JSON.stringify(docsToAdd));
+        }
+
+        function Init() {
+            try {
+                One2Teamapp.Utility.RestApiCall(One2Teamapp.Param.urlToGetconfigParam, "GET", One2Teamapp.Param.headerToGetconfigParam, function (callback) {
+                    if (callback.status && callback.status == 'error') {
+                        alert(One2Teamapp.Utility.customMessage.ConfigParamError);
+                    }
+                    else {
+                        One2Teamapp.Param.domain = callback.domain;
+                        One2Teamapp.Param.appWebUrl = callback.appWebUrl;
+                        One2Teamapp.Param.documentUrl = callback.documentUrl;
+                        One2Teamapp.Param.applicationId = callback.applicationId;
+                        One2Teamapp.Param.clientsecret = callback.clientSecret;
+                        One2Teamapp.Param.redirectUrl = callback.redirectUrl;
+                    }
+                });
+            }
+            catch (e) {
+                alert(One2Teamapp.Utility.customMessage.ConfigParamError);
+            }
+        }
+
+        document.addEventListener('visibilitychange', function () {
+            if (document.visibilityState === 'visible' && tempfolderUrl != null && localStorage.getItem("accesstoken") != null && localStorage.getItem("accesstoken") != "null" && localStorage.getItem("accesstoken") != "") {
+                GetItems(tempfolderUrl);
+            }
+        });
+
+        $("html").on("dragover", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $("#uploadfile").addClass('upload-area-highlight');
+        });
+
+        $("html").on("drop", function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $("#uploadfile").removeClass('upload-area-highlight');
+        });
+
+        $('.modal-dialog').on('mouseout', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $("#uploadfile").removeClass('upload-area-highlight');
+        });
+
+        $('.upload-area').on('dragenter', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $("#uploadfile").addClass('upload-area-highlight');
+        });
+
+        $('.upload-area').on('dragover', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $("#uploadfile").addClass('upload-area-highlight');
+        });
+
+        $('.upload-area').on('drop', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $("#uploadfile").removeClass('upload-area-highlight');
+            $("#wait").css("display", "block");
+            var filesToUpload = e.originalEvent.dataTransfer.files;
+            var currentFolder = One2Teamapp.AppViewModel.breadCrumbs()[One2Teamapp.AppViewModel.breadCrumbs().length - 1].url; // "/One2TeamDocuments/One2Team Docs/Images";
+
+            One2Teamapp.Utility.ReadAndUploadFiles(filesToUpload, currentFolder);
+        });
+
+        Init();
+
+        return {
+            docs: docs,
+            breadCrumbs: breadCrumbs,
+            GetItems: GetItems,
+            OnFolderClick: OnFolderClick,
+            OnBreadCrumbClick: OnBreadCrumbClick,
+            GetToken: GetToken,
+            GetRefreshToken: GetRefreshToken,
+            GetFoldersAndFiles: GetFoldersAndFiles,
+            AddToList: AddToList,
+            Logout: Logout,
+            UploadToSharepoint: UploadToSharepoint,
+            UploadToSPhere: UploadToSPhere,
+            CreateNewFolder: CreateNewFolder
+        };
+    })();
+    ko.applyBindings(One2Teamapp.AppViewModel);
+})();
---
+++ scripts/custom/utility.js
@@ -0,0 +1,224 @@
+﻿var One2Teamapp = One2Teamapp || {};
+
+One2Teamapp.Param = (function () {
+
+    var msLoginUrl = "https://login.microsoftonline.com/";
+    var urlToGetconfigParam = "https://api-devel.one2team.com/api/helpers/o365/settings";
+    var headerToGetconfigParam = {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ3eURmVTY3aGxJN1Z4UTc5UWRlakkzYktQbkVXNTV4RSIsImV4cCI6IjE1MDg4MzgzMjgiLCJpYXQiOiIxNTAxMDYyMzI4Iiwic3ViIjoiYXV0aDB8a2hhbGlmZTItbW9iaWxlL2FsdCIsInNjb3BlcyI6W119.8C5bkFog8tQPrpVswKFmpWp+G2wBkxN1k49iOYOiSQs="
+    };
+
+    var appWebUrl = "";
+    var documentUrl = "";
+    var applicationId = "";
+    var clientsecret = "";
+    var redirectUrl = "";
+    var domain = "";
+
+
+    //var appWebUrl = "https://advaiya.sharepoint.com";
+    //var documentUrl = "/One2TeamDocuments";
+    //var applicationId = "61d6e529-199a-402a-9da0-3f1300c50d61";
+    //var clientsecret = "FCsCCcGjkI8cM0LsVpiw9OpGTwTIfI71MIiMvOuMqrM=";
+    //var redirectUrl = "http://one2teamappintegration.azurewebsites.net/oauth.php";
+    //var domain = "advaiya.com";
+
+    var timeOutToGetAccessToken = 120000;
+
+    return {
+        appWebUrl: appWebUrl,
+        documentUrl: documentUrl,
+        applicationId: applicationId,
+        clientsecret: clientsecret,
+        redirectUrl: redirectUrl,
+        domain: domain,
+        urlToGetconfigParam: urlToGetconfigParam,
+        headerToGetconfigParam: headerToGetconfigParam,
+        timeOutToGetAccessToken: timeOutToGetAccessToken,
+        msLoginUrl: msLoginUrl
+    };
+
+})();
+
+One2Teamapp.Utility = (function () {
+    'use strict';
+
+    var customMessage = {
+        "ConfigParamError": "Unable to get Configuration parameters to proceed further with O365 Integration! ",
+        "newFolderError": "Unable to create a new folder, please try again! ",
+        "docLibOpenError": "Unable to access Office 365 Document library, please try again! ",
+        "noFolderUpload": "Folder cannot be uploaded! ",
+        "fileUploadError": "A error occurred during file upload! ",
+        "enterFolerName": "Enter Folder Name",
+        "folderNameError": "Folder name cannot be blank! "
+    };
+
+    function RestApiCall(url, type, header, callback) {
+        $.ajax({
+            url: One2Teamapp.Param.appWebUrl + url,
+            type: type,
+            crossDomain: true,
+            headers: header,
+            success: function (data) {
+                if (data.d)
+                    callback(data.d);
+                else
+                    callback(data);
+            },
+            error: function (xhr, statusText, err) {
+                var message = "";
+                if (xhr.responseText) {
+                    var response = JSON.parse(xhr.responseText);
+                    message = response ? response.error.message.value : statusText;
+                }
+                callback({ status: "error", errormessage: message });
+            }
+        });
+    }
+
+    function GetIcon(fileExtension) {
+        switch (fileExtension) {
+            case 'pdf': return 'images/icons/pdf.png';
+            case 'pst': case 'ost': return 'images/icons/outlook.png';
+            case 'vsd': case 'ost': return 'images/icons/visio.png';
+            case 'zip': return "images/icons/zip.png";
+            case 'rar': return "images/icons/rar.png";
+            case 'gz': return "images/icons/zip.png";
+            case 'swf': return "images/icons/swf.jpg";
+            case 'odt': return "images/icons/odt.png";
+            case 'wbk': case 'wiz': case 'doc': case 'dot': case 'docx': return 'images/icons/word.png';
+            case 'slk': case 'xla': case 'xlam': case 'xlc': case 'xld': case 'xlk':
+            case 'xll': case 'xlm': case 'xls': case 'xlsb': case 'xlsm': case 'xlt':
+            case 'xltm': case 'xlw': case 'xlsx': return 'images/icons/excel.png';
+            case 'pot': case 'potm': case 'ppa': case 'ppam': case 'pps': case 'ppsm':
+            case 'ppt': case 'pptm': case 'pwz': case 'ppsx': case 'pptx': return 'images/icons/powerpoint.png';
+            case 'ai': case 'eps': case 'ps': return "images/icons/ps.jpg";
+            case 'jar': return "images/icons/jar.jpg";
+            case 'rtf': return "images/icons/rtf.jpg";
+            case 'mp3': return "images/icons/mp3.png";
+            case 'jpe': case 'jpg': return 'images/icons/jpg.png';
+            case 'jpeg': return 'images/icons/jpeg.png';
+            case 'jpg': return "images/icons/jpg.png";
+            case 'psd': return "images/icons/psd.png";
+            case 'pnz': case 'png': return 'images/icons/png.png';
+            case 'gif': return 'images/icons/gif.png';
+            case 'tif': case 'tiff': return "images/icons/tiff.png";
+            case 'bmp': return 'images/icons/bmp.png';
+            case 'mpeg': return 'images/icons/mpeg.png';
+            case 'mpa': case 'mpe': case 'mpg': return "images/icons/mpg.png";
+            case 'mp4': case 'mp4v': return "images/icons/mp4.png";
+            case 'mpv2': case 'mp2': case 'mp2v': case 'm1v': case 'm2v': case 'mov': case 'mqv': return "images/icons/mov.jpg";
+            case 'htm': case 'html': case 'hxt': case 'shtml': return "images/icons/html.jpg";
+            default: return 'images/icons/file.png';
+        }
+    }
+
+    function ReadAndUploadFiles(files, serverRelativeUrlToFolder) {
+        $("#wait").css("display", "block");
+        var fileCount = files.length;
+        var filesUploaded = 0;
+
+        for (var i = 0; i < fileCount; i++) {
+            if (!files[i].type) {
+                alert(customMessage.noFolderUpload);
+                filesUploaded++;
+            }
+            else {
+                var getFile = getFileBuffer(i);
+                getFile.done(function (arrayBuffer, i) {
+                    var addFile = addFileToFolder(arrayBuffer, i);
+                    addFile.done(function (file, status, xhr) {
+                        filesUploaded++;
+                        if (fileCount == filesUploaded) {
+                            One2Teamapp.AppViewModel.GetItems(serverRelativeUrlToFolder);
+                            filesUploaded = 0;
+                            $("#wait").css("display", "none");
+                        }
+                    });
+                    addFile.fail(function (jqXHR, textStatus, errorThrown) {
+                        filesUploaded++;
+                        if (fileCount == filesUploaded)
+                            $("#wait").css("display", "none");
+                        FailHandler(jqXHR, textStatus, errorThrown);
+                    });
+                });
+                getFile.fail(function (jqXHR, textStatus, errorThrown, file) {
+                    filesUploaded++;
+                    if (fileCount == filesUploaded)
+                        $("#wait").css("display", "none");
+                    FailHandler(jqXHR, textStatus, errorThrown);
+                });
+            }
+        }
+
+
+        function getFileBuffer(i) {
+            var deferred = jQuery.Deferred();
+            var reader = new FileReader();
+            reader.onloadend = function (e) {
+                deferred.resolve(e.target.result, i);
+            }
+            reader.onerror = function (e) {
+                deferred.reject(e.target.error);
+            }
+            reader.readAsArrayBuffer(files[i]);
+            return deferred.promise();
+        }
+
+
+        function addFileToFolder(arrayBuffer, i) {
+            var index = i;
+            var fileName = files[index].name;
+
+            var fileCollectionEndpoint = One2Teamapp.Param.appWebUrl + "/_api/web/getfolderbyserverrelativeurl('" + serverRelativeUrlToFolder + "')/Files/Add(url='" + fileName + "', overwrite=true)";
+            return jQuery.ajax({
+                url: fileCollectionEndpoint,
+                type: "POST",
+                data: arrayBuffer,
+                processData: false,
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("accesstoken"),
+                    "accept": "application/json;odata=verbose",
+                    "X-RequestDigest": jQuery("#__REQUESTDIGEST").val(),
+                    "content-length": arrayBuffer.byteLength
+                }
+            });
+        }
+    }
+
+    function UploadDocument(currentFolder) {
+        $("#wait").css("display", "block");
+        var fileInput = jQuery('#file-3');
+        ReadAndUploadFiles(fileInput[0].files, currentFolder);
+    }
+
+    //Error handling
+    function FailHandler(jqXHR, textStatus, errorThrown) {
+
+        var message = "";
+        if (jqXHR.message) {
+            message = jqXHR.message;
+        }
+        if (jqXHR.responseText) {
+            var response = JSON.parse(jqXHR.responseText);
+            message = response ? response.error.message.value : textStatus;
+        }
+        alert(customMessage.fileUploadError + message);
+    }
+
+    return {
+        RestApiCall: RestApiCall,
+        GetIcon: GetIcon,
+        UploadDocument: UploadDocument,
+        ReadAndUploadFiles: ReadAndUploadFiles,
+        customMessage: customMessage
+    };
+
+})();
+
+$(document).ready(function () {
+    jQuery.support.cors = true;
+    $('.modal').draggable();
+});
+
+
